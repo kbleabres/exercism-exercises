4703606387a5ea4068a3cdde14df5789ae92675d
Kim Leabres <kim03anne@gmail.com>
2016-04-06 14:34:49 +0800

    [#2] Refactor compute method

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,10 +5,8 @@ class Hamming
     diff = 0
 
     if first_dna.length.eql? second_dna.length
-      (0..first_dna.length).each do |i|
-        if !first_dna[i].eql? second_dna[i]
-          diff += 1
-        end
+      first_dna.length.times do |i|
+        diff += 1 unless first_dna[i].eql? second_dna[i]
       end
 
       return diff
