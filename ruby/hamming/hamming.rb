3dc3646a44822c1cbc5d1e13c73746532fc14478
Kim Leabres <kim03anne@gmail.com>
2016-04-06 14:58:01 +0800

    [#2] Refactor compute method (3rd)

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,10 +2,8 @@ class Hamming
   VERSION = 1
 
   def self.compute(first_dna, second_dna)
-    diff = 0
     if first_dna.length.eql? second_dna.length
-      diff = first_dna.length.times.count { |i| !first_dna[i].eql? second_dna[i] }
-      return diff
+      return diff = first_dna.length.times.count { |i| !first_dna[i].eql? second_dna[i] }
     else
       raise ArgumentError
     end
